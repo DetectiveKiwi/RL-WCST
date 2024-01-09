97520937a58d384484f6621b77952f91a57b82d6
DetectiveKiwi <jbferre@uw.edu>
2024-01-08 21:07:38 -0800

    Moved moving average function to tools.py

---
+++ tools.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def moving_average(a, n):
+    """
+    Calculates the moving average of an array.
+    Function taken from Jaime here:
+    https://stackoverflow.com/questions/14313510/how-to-calculate-moving-average-using-numpy
+    
+    Parameters
+    --------------
+    a: array to be averaged
+    n: size of window
+    
+    Returns
+    --------------
+    Moving average
+    """
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
